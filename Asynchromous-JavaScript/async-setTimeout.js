7eed0851eefd718739b8f9f473df86d2b241913e
Naman <60322358+namanzzz@users.noreply.github.com>
2023-09-02 16:19:19 +0530

    add files via upload

---
+++ Asynchromous-JavaScript/async-setTimeout.js
@@ -0,0 +1,19 @@
+//Synchronous System
+//It runs code one by one from top to bottom 
+
+console.log('I ')
+console.log(' eat ')
+console.log(' icecream ')
+console.log(' with ')
+
+// setTimeout asynchronous
+// function
+
+const fun1 = setTimeout(()=>{
+  console.log('asynchronous function executed')
+},4000)
+
+
+console.log(' a ')
+console.log(' spoon ')
+
