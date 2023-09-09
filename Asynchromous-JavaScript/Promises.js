4d76720a9505ca58fa62259464853cc020d22d4a
Naman Matoliya <60322358+namanzzz@users.noreply.github.com>
2023-09-09 17:17:32 +0530

    Update Promises.js

--- Asynchromous-JavaScript/Promises.js
+++ Asynchromous-JavaScript/Promises.js
@@ -1,1 +1,20 @@
 
+
+const event = new Promise((resolve,reject) => {
+  let name="John";
+  if(name=="Naman"){
+    resolve(name);
+  }
+  else{
+    reject("Name was not Naman, name was: " + name);
+  }
+  
+});
+
+event.then((name)=>console.log(name)).catch((error)=>{
+    console.log(error);
+}).finally(()=>{
+  console.log("Promise resolved")
+})
+
+
