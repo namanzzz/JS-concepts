f001ad82f96ae8259546b7d438fa0fe3932159fd
Naman Matoliya <60322358+namanzzz@users.noreply.github.com>
2023-09-10 13:24:50 +0530

    Update async.js

--- Asynchromous-JavaScript/async.js
+++ Asynchromous-JavaScript/async.js
@@ -1,1 +1,46 @@
 
+let stocks = {
+  Fruits: ['strawberry', 'graps', 'banana', 'apple'],
+  liquid: ['water', 'ice'],
+  holder: ['cone', 'cup', 'stick'],
+  toppings: ['chocolate', 'peanuts']
+}
+
+let is_shop_open = true;
+
+// let order = () => { 
+//   return new Promise((resolve,reject)=>{
+    
+//     if(true){
+//       resolve();
+//     }
+//     else{
+//       reject();
+//     }  
+//   })
+// }
+
+// order()
+// .then()
+// .then()
+// .catch()
+// .finally()
+
+async function order(){
+  try{
+    await abc
+  }
+  catch(err){
+    console.log("abc doesn't exist", err)
+  }
+  finally{
+    console.log('finally block runs code anyways')
+  }
+
+}
+
+
+order().then(()=>{
+  console.log('kjddjfkdjfd');
+});
+
