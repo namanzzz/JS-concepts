3a219721bce46267f8513af4b08119802800d109
Naman Matoliya <60322358+namanzzz@users.noreply.github.com>
2023-09-10 15:56:36 +0530

    Update await.js

--- Asynchromous-JavaScript/await.js
+++ Asynchromous-JavaScript/await.js
@@ -1,1 +1,38 @@
 
+let stocks = {
+  Fruits: ['strawberry', 'graps', 'banana', 'apple'],
+  liquid: ['water', 'ice'],
+  holder: ['cone', 'cup', 'stick'],
+  toppings: ['chocolate', 'peanuts']
+}
+
+let is_shop_open = true;
+
+let toppings_choice = () => {
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+    resolve(
+      console.log('which toppings would u love ?')
+    );
+      
+    } ,3000)
+  })
+}
+
+async function kitchen() {
+  console.log("A");
+  console.log("B");
+  console.log("C");
+
+
+  await toppings_choice()
+
+  console.log('D')
+}
+
+  kitchen();
+  
+
+  console.log('doing the dishes')
+  console.log('cleaning the tables')
+  console.log('taking others orders')
